fed8860d18fa58b8e4424ee20d4b10b2f9a23009
rekFTC <derekyyf.96@gmail.com>
2025-09-09 09:44:13 -0700

    added panels & constants for pedro, autoObbyTest1 complete

---
+++ TeamCode/src/main/java/overcharged/opmodes/autoObbyTest1.java
@@ -0,0 +1,166 @@
+package overcharged.opmodes;
+
+import static overcharged.config.RobotConstants.TAG_A;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.RobotLog;
+
+import overcharged.components.Button;
+import overcharged.components.RobotMecanum;
+import overcharged.components.colorSensor;
+import overcharged.components.hslides;
+import overcharged.components.vSlides;
+
+import com.pedropathing.follower.Follower;
+
+
+@Autonomous(name = "obelisk move test", group = "dTest")
+public class autoObbyTest1 extends OpMode {
+    // Init
+    private RobotMecanum robot;
+    private DigitalChannel hlimitswitch;
+    private DigitalChannel vlimitswitch;
+    FtcDashboard dashboard = FtcDashboard.getInstance();
+    MultipleTelemetry telems;
+    private ElapsedTime pathTimer;
+
+    private int pathState;
+
+    private Follower follower;
+
+    private Limelight3A limelight;
+
+    private int obelisk = 0;
+
+    ObbyID obbyID = ObbyID.NONE;
+
+    private enum ObbyID {
+        ONE,
+        TWO,
+        THREE,
+        NONE
+    }
+
+    public void buildPoses() {
+
+    }
+
+    public void buildPaths() {
+
+    }
+
+    // TODO: HERE IS WHERE THE MAIN PATH IS
+    // Main pathing
+    public void autoPath() {
+        switch (pathState) {
+            case 10: //START
+                break;
+            case 100: //TODO: test
+                telems.addLine("CASE 100 - IN TEST CASE!!");
+                break;
+        }
+    }
+
+    // path setter
+    public void setPathState(int state){
+        pathState = state;
+        pathTimer.reset();
+        autoPath();
+    }
+
+    //loop de loop
+    @Override
+    public void loop() {
+
+    }
+
+    // initialize robot
+    @Override
+    public void init() {
+        telems = new MultipleTelemetry(dashboard.getTelemetry(), telemetry);
+        robot = new RobotMecanum(this, true, false);
+        pathTimer = new ElapsedTime();
+
+        limelight = hardwareMap.get(Limelight3A.class, "Ethernet Device");
+        telemetry.setMsTransmissionInterval(11);
+        limelight.pipelineSwitch(0);
+        initPath();
+        setPathState(10);
+    }
+
+    //loop de loop but initialized
+    @Override
+    public void init_loop() {
+        telemetry.update();
+        telemetry.addLine("in loop");
+        initPath();
+    }
+
+    // path setter
+    public void setInitState(int state){
+        pathState = state;
+        initPath();
+    }
+
+    public void initPath() {
+        switch (pathState) {
+            case 10: //START
+                limelight.start();
+                setInitState(11);
+                break;
+            case 11:
+                LLResult result = limelight.getLatestResult();
+                obelisk = result.getFiducialResults().get(0).getFiducialId();
+                if(obelisk == 21) {
+                    setInitState(111);
+                } else if(obelisk == 22) {
+                    setInitState(112);
+                } else if(obelisk == 23) {
+                    setInitState(113);
+                } else {
+                    telemetry.addLine("not yet");
+                }
+                break;
+            case 111:
+                obbyID = ObbyID.ONE;
+                telemetry.addLine("1");
+                break;
+            case 112:
+                obbyID = ObbyID.TWO;
+                telemetry.addLine("2");
+                break;
+            case 113:
+                obbyID = ObbyID.THREE;
+                telemetry.addLine("3");
+                break;
+            case 100: //TODO: test
+                telems.addLine("CASE 100 - IN TEST CASE!!");
+                break;
+        }
+    }
+
+
+
+
+    @Override
+    public void start() {
+
+    }
+
+    public static void waitFor(int milliseconds) { //Waitor Function (WARNING IT IS BAD)
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < milliseconds) {
+            // loop
+        }
+    }
+
+
+}
--- TeamCode/src/main/java/overcharged/pedroPathing/Constants.java
+++ TeamCode/src/main/java/overcharged/pedroPathing/Constants.java
@@ -1,19 +1,78 @@
 package overcharged.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+public class Constants {
+
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(5)
+            .forwardZeroPowerAcceleration(-39.7394)
+            .lateralZeroPowerAcceleration(-78.5414)
+            .useSecondaryTranslationalPIDF(true)
+            .useSecondaryHeadingPIDF(true)
+            .useSecondaryDrivePIDF(true)
+            .centripetalScaling(0.00036)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.15, 0, 0, 0))
+            .headingPIDFCoefficients(new PIDFCoefficients(0.95, 0, 0, 0))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(1, 0.000001, 0, 0.6, 0))
+            .secondaryTranslationalPIDFCoefficients(
+                    new PIDFCoefficients(0.19, 0, 0.02, 0)
+            )
+            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(2.5, 0, 0.05, 0))
+            .secondaryDrivePIDFCoefficients(
+                    new FilteredPIDFCoefficients(0.02, 0.00001, 0.0001, 0.6, 0)
+            );
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .leftFrontMotorName("driveLF")
+            .leftRearMotorName("driveLB")
+            .rightFrontMotorName("driveRF")
+            .rightRearMotorName("driveRB")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(65.5534)
+            .yVelocity(48.6396);
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(2.8346456693)
+            .strafePodX(3.4377953)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .yawScalar(1.0)
+            .encoderResolution(
+                    GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD
+            )
+            .customEncoderResolution(13.26291192)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.995,
+            500,
+            0.95,
+            1
+    );
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
                 .build();
     }
 }
+
