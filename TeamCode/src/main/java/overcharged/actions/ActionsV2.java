2c392e67da85b9ee87fc320cc1933e6b353ebeee
rekFTC <derekyyf.96@gmail.com>
2025-12-08 19:56:24 -0800

    theory new indexer & ActionsV2, Lift component made

---
+++ TeamCode/src/main/java/overcharged/actions/ActionsV2.java
@@ -0,0 +1,153 @@
+package overcharged.actions;
+
+import overcharged.components.RobotMecanum;
+
+public class ActionsV2 {
+    private RobotMecanum robot;
+
+
+
+    //fast shooting vars
+    private long shootTimer;
+    private int shootStep;
+    private int shootRepStep;
+
+    private int kickPause = 450;
+    private int retractPause = 250;
+    private int setIndPause = 250;
+
+    public void sortShootSys(RobotMecanum robot) {
+        this.robot = robot;
+        this.shootStep = 0;
+        this.shootRepStep = 0;
+        this.shootTimer = 0;
+    }
+
+    // @param int[3]
+    public void sortShootSeqOrder(int[] currentOrder) { //321
+        if (shootStep == 1 && System.currentTimeMillis() - shootTimer > 10) {
+            rollIndexer(currentOrder[0]);
+            shootStep += 1;
+            shootTimer = System.currentTimeMillis();
+        }
+        if (shootStep == 2 && System.currentTimeMillis() - shootTimer > kickPause) {
+            robot.kicker.setKick();
+            shootStep += 1;
+            shootTimer = System.currentTimeMillis();
+        }
+        if (shootStep == 3 && System.currentTimeMillis() - shootTimer > retractPause) {
+            robot.kicker.setInit();
+            shootStep += 1;
+            shootTimer = System.currentTimeMillis();
+        }
+        if (shootStep == 4 && System.currentTimeMillis() - shootTimer > setIndPause && shootRepStep == 0) {
+            rollIndexer(currentOrder[1]);
+            shootStep = 2;
+            shootRepStep += 1;
+            shootTimer = System.currentTimeMillis();
+        }
+        if (shootStep == 4 && System.currentTimeMillis() - shootTimer > setIndPause && shootRepStep == 1) {
+            rollIndexer(currentOrder[2]);
+            shootStep = 2;
+            shootRepStep += 1;
+            shootTimer = System.currentTimeMillis();
+        }
+        if (shootStep == 4 && System.currentTimeMillis() - shootTimer > 280 && shootRepStep == 2) {
+            robot.indexer.setTwo();
+            shootStep = 0;
+            shootRepStep = 0;
+            shootTimer = 0;
+        }
+    }
+
+    private void rollIndexer(int first) {
+        if(1 == first){
+            robot.indexer.setOne();
+        } else if (2 == first) {
+            robot.indexer.setTwo();
+        } else if (3 == first) {
+            robot.indexer.setThree();
+        }else{
+            robot.indexer.setOne();
+        }
+    }
+
+    public void startSortShoot() {
+        shootStep += 1;
+        shootTimer = System.currentTimeMillis();
+    }
+
+    //
+    private float goTo = robot.indexer.MAX;
+
+    private boolean shootNow = false;
+
+    public void fastShootSys(RobotMecanum robot) {
+        this.robot = robot;
+    }
+
+    public void fastShootAction(){
+        if(!shootNow) {
+            return;
+        } else if (shootNow) {
+            robot.indexer.setPosition(goTo);
+            shootNow = false;
+        }
+    }
+
+    public void fastShootStart() {
+        this.goTo = robot.indexer.getDirection();
+    }
+    //
+
+
+    //indexer(turntable) move
+    private enum indexerState {
+        INIT,
+        ONE,
+        TWO,
+        THREE,
+    }
+
+    indexerState indexerPos;
+    private boolean indMove;
+
+    public void indexerSys(RobotMecanum robot) {
+        this.robot = robot;
+        this.indexerPos = indexerState.INIT;
+        this.indMove = false;
+    }
+
+    public void indMoveSeq() {
+        if (!indMove) {
+            return;
+        }
+        switch (indexerPos) {
+            case INIT:
+                robot.indexer.setThree();
+                indexerPos = indexerState.THREE;
+                break;
+            case ONE:
+                robot.indexer.setTwo();
+                indexerPos = indexerState.INIT;
+                break;
+            case TWO:
+                robot.indexer.setThree();
+                indexerPos = indexerState.THREE;
+                break;
+            case THREE:
+                robot.indexer.setOne();
+                indexerPos = indexerState.ONE;
+                break;
+        }
+        indMove = false;
+    }
+
+    public void startIndMove() {
+        indMove = true;
+    }
+    //
+
+
+
+}
--- TeamCode/src/main/java/overcharged/components/indexer.java
+++ TeamCode/src/main/java/overcharged/components/indexer.java
@@ -9,11 +9,32 @@ public class indexer {
     public static final float TWO = 201f; //115f old
     public static final float THREE = 105f;//20f old
 
+    public static final float RONE = 10f;//211f old
+    public static final float RTWO = 201f; //115f old
+    public static final float RTHREE = 105f;//20f old
+
+    public static final float CRONE = 10f;//211f old
+    public static final float CRTWO = 201f; //115f old
+    public static final float CRTHREE = 105f;//20f old
+
+    public static final float MIN = 0f; //115f old
+    public static final float MAX = 255f;//20f old
+
+
+
     public indexer(HardwareMap hardwareMap) {
         indexer = new OcServo(hardwareMap, "indexer", TWO);
     }
     public void setPosition(float pos){
         indexer.setPosition(pos);
+    }
+
+    public float getDirection() {
+        if (MAX-indexer.getPosition() > MIN-indexer.getPosition()) {
+            return MAX;
+        } else {
+            return MIN;
+        }
     }
 
     public void setOne() { indexer.setPosition(ONE); }
